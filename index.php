9c77dfd26baab09e3b5c8a9fa65cd72ccde5960b
Muhammad Bilal <iambilal62@gmail.com>
2022-06-01 23:30:38 +0500

    Add testing reliance builders files to test

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include "includes/header.php"; ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-3">1</div>
+        <div class="col-3">2</div>
+        <div class="col-3">3</div>
+        <div class="col-3">4</div>
+    </div>
+</div>
+
+<?php include "includes/footer.php" ?>
